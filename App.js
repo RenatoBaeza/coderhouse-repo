d77d2040a05fd2972e73289b891aa5f703eaae01
RenatoBaeza <rntbzbg@gmail.com>
2023-06-27 19:05:32 -0400

    list added

--- App.js
+++ App.js
@@ -1,13 +1,49 @@
-import { StyleSheet, Text, View, TextInput, Button, StatusBar, SafeAreaView } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, SafeAreaView } from 'react-native';
 import { styles } from './styles';
 
 export default function App() {
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  const onHandlerChangeText = (text) => {
+    setTask(text);
+  };
+
+  const onHandlerCreateTask = () => {
+    if (task !== '') {
+      setTasks((prevTasks) => [
+        ...prevTasks,
+        { id: Date.now().toString(), value: task },
+      ]);
+      setTask('');
+    }
+  };
+
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
         <View style={styles.inputContainer}>
-          <TextInput style={styles.input} placeholder='Add new tasks...' placeholderTextColor={'gray'} autoCapitalize='none' cursorColor='#424DEE' selectionColor={'red'}/>
-          <Button title='Create' color='#424D9E' />
+          <TextInput
+            style={styles.input}
+            placeholder="Add new tasks..."
+            placeholderTextColor="gray"
+            autoCorrect={false}
+            autoCapitalize="none"
+            cursorColor="#424DEE"
+            selectionColor="red"
+            onChangeText={onHandlerChangeText}
+            value={task}
+          />
+          <Button title="ADD" color="#424D9E" onPress={onHandlerCreateTask} disabled={task.length === 0} />
+        </View>
+
+        <View style={styles.listContainer}>
+          {tasks.map((item) => (
+            <View style={styles.containerItem} key={item.id}>
+              <Text style={styles.listItem}>{item.value}</Text>
+            </View>
+          ))}
         </View>
       </View>
     </SafeAreaView>
--- styles.js
+++ styles.js
@@ -1,27 +1,56 @@
 import { StyleSheet, StatusBar } from "react-native";
 
 export const styles = StyleSheet.create({
-    safeArea: {
-      flex: 1,
+  safeArea: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    marginHorizontal: 20,
+    marginTop: StatusBar.currentHeight,
+    paddingTop: StatusBar.currentHeight,
+    marginBottom: 20
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  input: {
+    flex: 0.95,
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    color: '#212121',
+    borderColor: '#424D9E',
+    fontSize: 14,
+  },
+  listContainers: {
+    marginHorizontal: 20,
+    gap: 15,
+    marginTop: 40
+  },
+  containerItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
     },
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      marginHorizontal: 20,
-      paddingTop: StatusBar.currentHeight
-    },
-    inputContainer: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-    },
-    input: {
-      flex: 0.95,
-      borderWidth: 1,
-      borderRadius: 10,
-      paddingHorizontal: 10,
-      color: '#212121',
-      borderColor: '#424D9E',
-      fontSize: 14,
-    }
-  });
-  
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+
+    elevation: 3,
+  },
+  listItem: {
+    marginTop: 5,
+    fontSize: 15,
+    fontWeight: 'bold',
+    marginBottom: 5
+  }
+});
