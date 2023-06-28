ce69b97a5b757f65e53d295be2af59d62817a0c8
RenatoBaeza <rntbzbg@gmail.com>
2023-06-27 20:07:50 -0400

    modals

--- styles.js
+++ styles.js
@@ -8,9 +8,7 @@ export const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     marginHorizontal: 20,
-    marginTop: StatusBar.currentHeight,
-    paddingTop: StatusBar.currentHeight,
-    marginBottom: 20
+    paddingTop: StatusBar.currentHeight
   },
   inputContainer: {
     flexDirection: 'row',
@@ -22,21 +20,20 @@ export const styles = StyleSheet.create({
     borderRadius: 10,
     paddingHorizontal: 10,
     color: '#212121',
-    borderColor: '#424D9E',
     fontSize: 14,
   },
-  listContainers: {
-    marginHorizontal: 20,
-    gap: 15,
-    marginTop: 40
+  listContainer: {
+    marginTop: 25,
   },
   containerItem: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
     paddingVertical: 20,
+    backgroundColor: '#D4D7ED',
     paddingHorizontal: 10,
     borderRadius: 10,
+    color: '#212121',
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
@@ -44,13 +41,50 @@ export const styles = StyleSheet.create({
     },
     shadowOpacity: 0.22,
     shadowRadius: 2.22,
-
     elevation: 3,
   },
   listItem: {
-    marginTop: 5,
     fontSize: 15,
     fontWeight: 'bold',
-    marginBottom: 5
-  }
+    color: '#212121',
+  },
+  list: {
+    gap: 15,
+    paddingBottom: 20,
+  },
+  icon: {
+    color: 'red',
+    fontSize: 16,
+  },
+  modalContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 60,
+    paddingVertical: 20,
+  },
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  modalDetailContainer: {
+    paddingVertical: 20,
+  },
+  modalDetailMessage: {
+    fontSize: 14,
+    color: '#212121',
+  },
+  selectedTask: {
+    fontSize: 14,
+    color: '#212121',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    paddingVertical: 10,
+  },
+  modalButtonContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-around',
+    marginHorizontal: 20,
+  },
 });
